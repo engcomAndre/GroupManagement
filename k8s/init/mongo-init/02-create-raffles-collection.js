b93b0013d69a6d729fec36af11cca38d0ea6912b
Andre Vieira <sgavsnake@gmail.com>
2025-07-29 10:56:46 -0300

    Remove k8s infrastructure files - keep only GroupManagement content

--- k8s/init/mongo-init/02-create-raffles-collection.js
+++
@@ -1,6 +0,0 @@
-print('=== Criando collection raffles no banco raffles_db ===');
-
-db = db.getSiblingDB('raffles_db');
-db.createCollection('raffles');
-
-print('=== Collection criada raffles no banco raffles_db ===');
